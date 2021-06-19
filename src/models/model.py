6cb9d67970ae4f42d824363d3c5a583dab84338e
Chrypapado <chrypapadopoulos@gmail.com>
2021-06-19 15:45:55 +0200

    Model Ready

--- src/models/model.py
+++ src/models/model.py
@@ -3,43 +3,13 @@ from torch import nn
 import torch.nn.functional as F
 import torchvision.models as models
 
-def accuracy(outputs, labels):
-    _, preds = torch.max(outputs, dim=1)
-    return torch.tensor(torch.sum(preds == labels).item() / len(preds))
-
-class ImageClassificationBase(nn.Module):
-    def training_step(self, batch):
-        images, labels = batch 
-        out = self(images)                  
-        loss = F.cross_entropy(out, labels) 
-        return loss
-    
-    def validation_step(self, batch):
-        images, labels = batch 
-        out = self(images)                    
-        loss = F.cross_entropy(out, labels)   
-        acc = accuracy(out, labels)           
-        return {'val_loss': loss.detach(), 'val_acc': acc}
-        
-    def validation_epoch_end(self, outputs):
-        batch_losses = [x['val_loss'] for x in outputs]
-        epoch_loss = torch.stack(batch_losses).mean()   
-        batch_accs = [x['val_acc'] for x in outputs]
-        epoch_acc = torch.stack(batch_accs).mean()      
-        return {'val_loss': epoch_loss.item(), 'val_acc': epoch_acc.item()}
-    
-    def epoch_end(self, epoch, result):
-        print("Epoch {}: train_loss: {:.4f}, val_loss: {:.4f}, val_acc: {:.4f}".format(
-            epoch+1, result['train_loss'], result['val_loss'], result['val_acc']))
-
-class ResNet(ImageClassificationBase):
+class ResNet(nn.Module):
     def __init__(self):
         super().__init__()
         #Pretrained Model
         self.network = models.resnet50(pretrained=True)
         num_ftrs = self.network.fc.in_features
         self.network.fc = nn.Linear(num_ftrs, 6)
-        #self.network.fc = nn.Linear(num_ftrs, len(train_set.classes))
 
     def forward(self, xb):
         return torch.sigmoid(self.network(xb))
---
+++ src/models/train_model.py
@@ -0,0 +1,114 @@
+#Import Libraries and Modules
+import sys
+import argparse
+from pathlib import Path
+import torch
+import torch.nn as nn
+from torch.utils.data.dataloader import DataLoader
+from model import ResNet
+project_dir = Path(__file__).resolve().parents[2]
+sys.path.insert(0, str(project_dir / 'src/data'))
+from make_dataset import main as dataset
+
+class TrainOREvaluate(object):
+    """ Helper class that will help launch class methods as commands
+        from a single script
+    """
+    def __init__(self):
+        parser = argparse.ArgumentParser(
+            description="Script for either training or evaluating",
+            usage="python train_model.py <command>"
+        )
+        parser.add_argument("command", help="Subcommand to run")
+        args = parser.parse_args(sys.argv[1:2])
+        if not hasattr(self, args.command):
+            print('Unrecognized command')
+            parser.print_help()
+            exit(1)
+        getattr(self, args.command)()
+
+    def train(self):
+        print("Training day and night")
+        # Arguments
+        parser = argparse.ArgumentParser(description='Training arguments')
+        parser.add_argument('--lr', default=5.5e-5)
+        parser.add_argument('--batch_size', default=64)
+        parser.add_argument('--num_epochs', type=int, default=10)
+        args = parser.parse_args(sys.argv[2:])
+        print(args)
+        # Load Data
+        train_set, test_set, val_set = dataset()
+        # Set Device and Model Configurations 
+        device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+        print(device)
+        model = ResNet()
+        model.to(device)
+        criterion = nn.CrossEntropyLoss()
+        optimizer = torch.optim.Adam(model.parameters(), lr=args.lr)
+        train_dl = DataLoader(train_set, args.batch_size, shuffle = True, num_workers = 4, pin_memory = True)
+        val_dl = DataLoader(val_set, args.batch_size * 2, num_workers = 4, pin_memory = True)
+        # Training
+        for epoch in range(args.num_epochs):
+            print('Epoch '+ str(epoch + 1))
+            model.train()
+            for images, labels in train_dl:
+                images, labels = images.to(device), labels.to(device)
+                output = model(images)
+                loss = criterion(output, labels)
+                loss.backward()
+                optimizer.step()
+                optimizer.zero_grad()
+            
+            accuracy = 0
+            counter = 0
+            with torch.no_grad():
+                model.eval()
+                for images, labels in val_dl:
+                    images, labels = images.to(device), labels.to(device)
+                    ps = torch.exp(model(images))
+                    top_p, top_class = ps.topk(1, dim=1)
+                    equals = top_class == labels.view(*top_class.shape)
+                    counter += 1
+                    accuracy += torch.mean(equals.type(torch.FloatTensor))
+                accuracy = accuracy / counter
+                print('Accuracy: ' + str(accuracy*100) + '%')
+            torch.save(model.state_dict(), 
+                       project_dir.joinpath('models/model' + str(epoch) + '.pth'))
+
+    def evaluate(self):
+        print("Evaluating until hitting the ceiling")
+        # Arguments
+        parser = argparse.ArgumentParser(description='Training arguments')
+        parser.add_argument('--load_model_from', default='model0')
+        parser.add_argument('--batch_size', default=64)
+        args = parser.parse_args(sys.argv[2:])
+        print(args)
+        # Load Model
+        model_path = str(project_dir.joinpath('./models')) + '/' + args.load_model_from + '.pth'
+        # Load Data
+        train_set, test_set, val_set = dataset()
+        # Set Device and Model Configurations 
+        device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+        print(device)
+        model = ResNet()
+        model.to(device)
+        test_dl = DataLoader(test_set, args.batch_size*2, num_workers = 4, pin_memory = True)
+        print(device)
+        dict_ = torch.load(model_path)
+        model.load_state_dict(dict_)
+        accuracy = 0
+        counter = 0
+        with torch.no_grad():
+            model.eval()
+            for images, labels in test_dl: 
+                images, labels = images.to(device), labels.to(device)
+                ps = torch.exp(model(images))
+                top_p, top_class = ps.topk(1, dim=1)
+                equals = top_class == labels.view(*top_class.shape)
+                counter += 1
+                accuracy += torch.mean(equals.type(torch.FloatTensor))
+            accuracy = accuracy / counter
+            print(f'Accuracy: {accuracy.item()*100}%')
+
+if __name__ == '__main__':
+    TrainOREvaluate()
