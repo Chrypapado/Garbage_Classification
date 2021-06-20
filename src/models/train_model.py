ace246ebd68b686719de2b58dcf234c66a079492
Chrypapado <chrypapadopoulos@gmail.com>
2021-06-20 13:10:13 +0200

    Insert wandb

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -2,6 +2,7 @@
 import sys
 import argparse
 from pathlib import Path
+import wandb
 import torch
 import torch.nn as nn
 from torch.utils.data.dataloader import DataLoader
@@ -29,12 +30,13 @@ class TrainOREvaluate(object):
         # Arguments
         parser = argparse.ArgumentParser(description='Training arguments')
         parser.add_argument('--lr', default=5.5e-5)
-        parser.add_argument('--batch_size', default=64)
+        parser.add_argument('--batch_size', default=32)
         parser.add_argument('--num_epochs', type=int, default=10)
         args = parser.parse_args(sys.argv[2:])
         print(args)
         # Load Data
         train_set, test_set, val_set = dataset()
+        classes = ['Glass', 'Paper',  'Cardboard', 'Plastic',  'Metal',  'Trash']
         # Set Device and Model Configurations 
         device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
         print(device)
@@ -43,31 +45,58 @@ class TrainOREvaluate(object):
         criterion = nn.CrossEntropyLoss()
         optimizer = torch.optim.Adam(model.parameters(), lr=args.lr)
         train_dl = DataLoader(train_set, args.batch_size, shuffle = True, num_workers = 4, pin_memory = True)
-        val_dl = DataLoader(val_set, args.batch_size * 2, num_workers = 4, pin_memory = True)
-        # Training
+        val_dl = DataLoader(val_set, args.batch_size * 2, shuffle = True, num_workers = 4, pin_memory = True)
+        # Initialize wandb
+        wandb.login()
+        wandb.init(project='testing', config=args, dir= str(project_dir.joinpath('./reports')))
+        wandb.watch(model, log_freq=100)
         for epoch in range(args.num_epochs):
-            print('Epoch '+ str(epoch + 1))
+            # Training
+            train_accuracy = 0
             model.train()
-            for images, labels in train_dl:
+            for (counter, (images, labels)) in enumerate(train_dl):
                 images, labels = images.to(device), labels.to(device)
                 output = model(images)
+                _, predictions = torch.max(output, dim=1)
                 loss = criterion(output, labels)
                 loss.backward()
                 optimizer.step()
                 optimizer.zero_grad()
-            accuracy = 0
-            counter = 0
+                train_acc = torch.tensor(torch.sum(predictions == labels).item() / len(predictions))
+                wandb.log({"Training Accuracy (per batch)": train_acc})
+                wandb.log({"Training Loss": loss})
+                train_accuracy += train_acc
+            epoch_train_acc = train_accuracy.item() / (counter+1)
+            wandb.log({"Training Accuracy (per epoch)": epoch_train_acc})
+            print('(Epoch {}) Training accuracy: {:.2f}%'.format(epoch+1, epoch_train_acc*100))
+            # Evaluation 
             with torch.no_grad():
+                val_accuracy = 0
+                class_acc = {}
+                correct_pred_list = [0 for _ in range(6)]
+                total_pred_list = [0 for _ in range(6)]
                 model.eval()
-                for images, labels in val_dl:
+                for (counter, (images, labels)) in enumerate(val_dl):
                     images, labels = images.to(device), labels.to(device)
-                    ps = torch.exp(model(images))
-                    top_p, top_class = ps.topk(1, dim=1)
-                    equals = top_class == labels.view(*top_class.shape)
-                    counter += 1
-                    accuracy += torch.mean(equals.type(torch.FloatTensor))
-                accuracy = accuracy / counter
-                print('Accuracy: ' + str(accuracy*100) + '%')
+                    outputs = model(images)
+                    _, predictions = torch.max(outputs, dim=1)
+                    equals = predictions == labels
+                    val_loss = criterion(outputs, labels)
+                    val_acc = torch.tensor(torch.sum(predictions == labels).item() / len(predictions))
+                    wandb.log({"Validation Accuracy (per batch)": val_acc})
+                    wandb.log({"Validation Loss": val_loss})
+                    val_accuracy += val_acc
+                    for label, prediction in zip(labels, predictions):
+                        if label == prediction:
+                            correct_pred_list[label] += 1
+                        total_pred_list[label] += 1
+                epoch_val_acc = val_accuracy.item() / (counter+1)
+                wandb.log({"Validation Accuracy (per epoch)": epoch_val_acc})
+                print('(Epoch {}) Validation accuracy: {:.2f}%'.format(epoch+1, epoch_val_acc*100))
+            for i in range(len(classes)):
+                print('Accuracy of %5s : %.2f %%'%(classes[i], 100 * correct_pred_list[i] / total_pred_list[i]))
+                class_acc["Accuracy of %5s" % (classes[i])] = 100 * correct_pred_list[i] / total_pred_list[i]
+            wandb.log(class_acc)
             torch.save(model.state_dict(), 
                        project_dir.joinpath('models/model' + str(epoch) + '.pth'))
 
